112e416a43e1520703ec6b02e6e92d7a4b12b74f
Harold Rangel Charris <harold.rangel@koombea.com>
2012-03-12 13:54:17 -0500

    using api google map

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,9 +1,12 @@
 class VenuesController < ApplicationController
+
+
 	def search 
 		#nothinhg to do here  
 	end
 
-	def find 
+	def find		
+
 		if params[:llAcc] == ""
 			params[:llAcc] = 0
 		end
@@ -18,6 +21,6 @@ class VenuesController < ApplicationController
 		parameters = Hash.new(0)
 		parameters[:sort] = "recent"
 		parameters[:limit] = 100
-		@tips = Foursquare_client.get_tips(params[:id], parameters)
+		@tips = Foursquare_client.get_tips(params[:id] , parameters)
 	end
 end
